531b62f90b9f80728b629d1951cadf79b5bc185c
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-12-09 11:42:02 -0600

    added npm inquirer, mysql2, dotenv, console.table, created db directory with schema and seeds files, created server, gitignore and env files

---
+++ server.js
@@ -0,0 +1,9 @@
+// Importing files
+const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+// Password Protect imported file
+const dotenv = require('dotenv');
+
+
