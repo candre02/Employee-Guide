373aacb14ddd45f8253dbee0b9188b2b4b39084f
Crystal Andre <81876258+candre02@users.noreply.github.com>
2021-12-09 20:10:13 -0600

    in server file created a department, get all departments, get a single department, created delete routes

--- server.js
+++ server.js
@@ -47,6 +47,74 @@ const db = mysql.createConnection(
             console.log(result);
     });
 
+    // Create a department
+    const sql = `INSERT INTO department (id, dept_name)
+                    VALUES (?,?,?,?)`;
+
+    const params = [ , ''];
+    
+    db.query(sql,params, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(result);
+    });
+
+    // Get all departments
+    app.get('/db/seeds', (req, res) => {
+        const sql = `SELECT * FROM department`;
+
+        db.query(sql, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: rows
+            });
+        });
+    });
+
+    // Get a single department
+    app.get('/db/seeds/:id', (req, res) => {
+        const sql = `SELECT * FROM department WHERE id = ?`;
+        const params = [req.params.id];
+
+        db.query(sql, params, (err, row) => {
+            if (err) {
+                res.status(400).json({ error: err.message });
+                return;
+            }
+            res.json({
+                message: 'success',
+                data: row
+            });
+        });
+    });
+
+    // Delete a department
+    app.delete('/db/seeds/:id', (req, res) => {
+        const sql = `DELETE FROM department WHERE id = ?`;
+        const params = [req.params.id];
+
+        db.query(sql, params, (err, result) => {
+            if (err) {
+                res.statusMessage(400).json({ error: res.message });
+            } else if (!result.deletedRow) {
+                res.json({
+                    message: 'Department not found'
+                });
+            } else {
+                res.json({
+                    message: 'deleted',
+                    changes: result.deletedRow,
+                    id: req.params.id
+                });
+            }
+        });
+    });
+
     // Query database and select from
     db.query('SELECT * FROM departments', function (err, results) {
         console.log(results);
